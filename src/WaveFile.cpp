9b75dfb1b481f0b9fdb391f51e070500a65b3c85
Leslie Yip <leslieyip02@gmail.com>
2024-05-23 23:44:28 +0800

    Add wave header parser

---
+++ src/WaveFile.cpp
@@ -0,0 +1,70 @@
+#include "WaveFile.hpp"
+
+#include <fstream>
+#include <iostream>
+
+WaveFile::WaveFile(std::string filename)
+{
+    // WAVE file format: http://soundfile.sapp.org/doc/WaveFormat/
+    std::ifstream byteStream(filename, std::ios::binary);
+    char headerBuffer[HEADER_SIZE];
+    byteStream.read(headerBuffer, HEADER_SIZE);
+
+    char* headerPointer = headerBuffer;
+    // TODO: validate "RIFF", "WAVE", "fmt " and "data"
+    headerPointer += 0; // 0:  ChunkID
+    headerPointer += 4; // 4:  ChunkSize
+    headerPointer += 4; // 8:  Format
+
+    headerPointer += 4; // 12: Subchunk1ID
+    headerPointer += 4; // 16: Subchunk1Size
+    headerPointer += 4; // 20: AudioFormat
+    headerPointer += 2; // 22: NumChannels
+    numChannels = littleEndian(headerPointer, 2);
+    headerPointer += 2; // 24: SampleRate
+    sampleRate = littleEndian(headerPointer, 4);
+    headerPointer += 4; // 28: ByteRate
+    headerPointer += 4; // 32: BlockAlign
+    headerPointer += 2; // 34: BitsPerSample
+    bitsPerSample = littleEndian(headerPointer, 2);
+    int bytesPerSample = bitsPerSample / 8;
+
+    headerPointer += 2; // 36: Subchunk2ID
+    headerPointer += 4; // 40: Subchunk2Size
+    uint32_t dataSize = littleEndian(headerPointer, 4);
+    numSamples = dataSize / numChannels / bytesPerSample;
+
+    // sanity check
+    std::cout << "numChannels: " << numChannels << "\n";
+    std::cout << "sampleRate: " << sampleRate << "\n";
+    std::cout << "bitsPerSample: " << bitsPerSample << "\n";
+    std::cout << "numSamples: " << numSamples << "\n";
+}
+
+uint32_t WaveFile::littleEndian(char* bytes, size_t size)
+{
+    // least significant byte at smallest address
+    uint32_t value = 0;
+    for (int i = 0; i < size; i++)
+    {
+        // bitmask is necessary to prevent sign extension
+        uint32_t offset = i * 8;
+        uint32_t mask = 255 << offset;
+        value |= (bytes[i] << offset) & mask;
+    }
+    return value;
+}
+
+uint32_t WaveFile::bigEndian(char* bytes, size_t size)
+{
+    // mosst significant byte at biggest address
+    uint32_t value = 0;
+    for (int i = 0; i < size; i++)
+    {
+        // bitmask is necessary to prevent sign extension
+        uint32_t offset = (size - i - 1) * 8;
+        uint32_t mask = 255 << offset;
+        value |= bytes[i] << offset;
+    }
+    return value;
+}
---
+++ src/WaveFile.hpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class WaveFile
+{
+public:
+    static const size_t HEADER_SIZE = 44;
+
+    WaveFile(std::string filename);
+
+private:
+    // uint32_t since header data can contain up to 4 bytes = 32 bits
+    uint32_t numChannels;
+    uint32_t sampleRate;
+    uint32_t bitsPerSample;
+    uint32_t numSamples;
+
+    uint32_t littleEndian(char* bytes, size_t size);
+    uint32_t bigEndian(char* bytes, size_t size);
+};
